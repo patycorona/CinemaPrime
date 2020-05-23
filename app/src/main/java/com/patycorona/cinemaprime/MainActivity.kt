8449061914ad1aec01216654d6fdbffcfb500738
patricia corona <pchz.mxtobn@gmail.com>
2020-05-23 09:51:23 -0500

    proyecto inicial

---
+++ app/src/main/java/com/patycorona/cinemaprime/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.patycorona.cinemaprime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
